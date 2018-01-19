1c4e5a1ec2430b6bb641430bbefc552c6d91eb5f
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2018-01-19 19:29:29 +0900

    Show diff of data.json

--- update-catalog.ps1
+++ update-catalog.ps1
@@ -1,2 +1,14 @@
-mkdir data -Force
-Invoke-WebRequest http://comip.jp/15/comics/data.json -OutFile (Join-Path data data.json)
+mkdir data -Force | Out-Null
+$catalogPath = (Join-Path data data.json)
+
+$hasOldJson = Test-Path $catalogPath
+if ($hasOldJson) {
+  $oldJson = Get-Content -Encoding UTF8 -Raw $catalogPath | ConvertFrom-Json
+}
+
+Invoke-WebRequest http://comip.jp/15/comics/data.json -OutFile $catalogPath
+
+if ($hasOldJson) {
+  $newJson = Get-Content -Encoding UTF8 -Raw $catalogPath | ConvertFrom-Json
+  Compare-Object $oldJson $newJson -Property key,title,page-num
+}
